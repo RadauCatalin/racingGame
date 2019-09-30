0f236c7e1736d7ba87bf98e2df36bccc65dc74a3
Catalin <catalin.radau@yahoo.com>
2019-09-30 19:07:03 +0300

    Studied while oops

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -22,12 +22,26 @@ public class Game {
         Track selectedTrak = getSelectedTrackFromUser();
 
         initializeCompetitors();
-        // for-each or enhanced for loop
-        for (Vehicle vehicle : competitors){
-            double speed = getAccelerateSpeedFromUser();
-            vehicle.accelerate(speed, 1);
-        }
+        boolean winnerNotKnown = true;
+        int competitorsWithoutFuel = 0;
 
+        while( winnerNotKnown && competitorsWithoutFuel < competitors.size()) {
+
+            // for-each or enhanced for loop
+            for (Vehicle vehicle : competitors) {
+                double speed = getAccelerateSpeedFromUser();
+                vehicle.accelerate(speed, 1);
+
+                if (selectedTrak.getLenght() <= vehicle.getTraveledDistance()){
+                    winnerNotKnown = false;
+                    System.out.println("The winner is " + vehicle.getName());
+                    break;
+                }
+                if (vehicle.getFuelLevel() <= 0) {
+                    competitorsWithoutFuel++;
+                }
+            }
+        }
     }
     private double getAccelerateSpeedFromUser(){
         System.out.println("Please enter acceleration speed");
